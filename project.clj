e3d003b91e5261c998536485dd022b5f29a5099c
Michael Bauer <mihi@lo-res.org>
2013-12-06 17:47:01 +0100

    bump project to 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fuzzy-string "0.1.2"
+(defproject fuzzy-string "0.1.3-SNAPSHOT"
   :description "Fuzzy-string matching write description"
   :url "http://github.com/mihi-tr/fuzzy-string"
   :license {:name "Eclipse Public License"
